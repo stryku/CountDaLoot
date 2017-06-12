755658f71112db62897372435cd94d469ad26f9c
stryku <stryku2393@gmail.com>
2017-06-12 07:59:12 +0200

    using LootData and ILootObserver in test

--- tests/loot/LootTabReaderTests.cpp
+++ tests/loot/LootTabReaderTests.cpp
@@ -1,13 +1,15 @@
+#include "loot/ILootObserver.hpp"
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <vector>
 
 namespace tests
 {
-    class LootObserverMock : public cdl::ILootObserver
+    class LootObserverMock : public cdl::loot::ILootObserver
     {
     public:
-        MOCK_METHOD1(notify, void(std::vector<Loot>))
+        MOCK_METHOD1(notify, void(const std::vector<cdl::loot::LootData>&))
     };
 }
 
