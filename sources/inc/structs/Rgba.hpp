3889bb538036ad81cef7c78e0ab9138a2694a9f0
stryku <stryku2393@gmail.com>
2017-06-14 05:11:29 +0200

    introduce Rgba

---
+++ sources/inc/structs/Rgba.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace cdl
+{
+    struct Rgba
+    {
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        uint8_t a;
+
+        bool operator==(const Rgba& o) const
+        {
+            return o.r == r && o.g == g && o.b == b && o.a == a;
+        }
+
+        bool operator!=(const Rgba& o) const
+        {
+            return !(*this == o);
+        }
+    };
+}
