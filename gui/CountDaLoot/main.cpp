4352bdc0cf3fc94bcea1a6f1ca56e541ca757ede
stryku <stryku2393@gmail.com>
2017-06-15 06:25:46 +0200

    fixed load() method

--- gui/CountDaLoot/main.cpp
+++ gui/CountDaLoot/main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include "graphics/Image.hpp"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    auto image = cdl::graphics::Image::load("C:/moje/tmp/git/tlc/repo/data/img/loot_active.bmp");
+
+
     w.show();
 
     return a.exec();
