483e50131ff21db4a4a04115807bd44b1016960f
stryku <stryku2393@gmail.com>
2017-06-21 07:31:18 +0200

    added methods to header

--- sources/inc/loot/tab/LootTabFinder.hpp
+++ sources/inc/loot/tab/LootTabFinder.hpp
@@ -2,12 +2,27 @@
 
 namespace cdl
 {
+    struct Offset;
+    struct Rect;
+
+    namespace graphics
+    {
+        struct Image;
+    }
+
     namespace loot
     {
-        class LootTabFinder
+        namespace tab
         {
-        public:
+            struct LootTabCoordinates;
 
-        };
+            class LootTabFinder
+            {
+            public:
+                LootTabCoordinates findCoordinates(const graphics::Image& screen) const;
+                Offset findHeaderPos(const graphics::Image& screen) const;
+                Rect findAreaRect(const graphics::Image& screen) const;
+            };
+        }
     }
 }
