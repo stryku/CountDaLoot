b4f6deda62f8a34325eb8dffb46b73acd67565d9
stryku <stryku2393@gmail.com>
2017-07-02 16:42:37 +0200

    added Long Monster Name test

--- tests/loot/parse/LootLineParser.cpp
+++ tests/loot/parse/LootLineParser.cpp
@@ -9,32 +9,67 @@ namespace tests
     {
         namespace parse
         {
-            struct LootLineParserTestStruct
+            namespace a_and_an
             {
-                const std::string line;
-                const std::string expectedMonster;
-            };
+                struct LootLineParserTestStruct
+                {
+                    const std::string line;
+                    const std::string expectedMonster;
+                };
 
-            struct LootLineParserTest : testing::Test, testing::WithParamInterface<LootLineParserTestStruct> {};
+                struct LootLineParserTest : testing::Test, testing::WithParamInterface<LootLineParserTestStruct> {};
 
-            TEST_P(LootLineParserTest, aAndAnMonster)
-            {
-                const auto param = GetParam();
-                const auto result = cdl::loot::parse::LootLineParser{}.parse(param.line);
-                ASSERT_EQ(result.monster, param.expectedMonster);
+                TEST_P(LootLineParserTest, aAndAnMonster)
+                {
+                    const auto param = GetParam();
+                    const auto result = cdl::loot::parse::LootLineParser{}.parse(param.line);
+                    ASSERT_EQ(result.monster, param.expectedMonster);
+                }
+
+                INSTANTIATE_TEST_CASE_P(LootLineParser, LootLineParserTest,
+                                        testing::Values(
+                                        LootLineParserTestStruct{
+                                            std::string{ "Loot of a monster: " },
+                                            std::string{ "monster" }
+                                        },
+                                        LootLineParserTestStruct{
+                                            std::string{ "Loot of an monster: " },
+                                            std::string{ "monster" }
+                                        }));
             }
 
-            INSTANTIATE_TEST_CASE_P(LootLineParser, LootLineParserTest,
-                                    testing::Values(
-                                    LootLineParserTestStruct{
-                                        std::string{ "Loot of a monster: " },
-                                        std::string{ "monster" }
-                                    },
-                                    LootLineParserTestStruct{
-                                        std::string{ "Loot of an monster: " },
-                                        std::string{ "monster" }
-                                    }));
+            namespace monster_name
+            {
+                struct MonsterNameTestStruct
+                {
+                    const std::string line;
+                    const std::string expectedMonsterName;
+                };
+
+                struct MonsterNameTest : testing::Test, testing::WithParamInterface<MonsterNameTestStruct> {};
+
+                TEST_P(MonsterNameTest, MonsterName)
+                {
+                    const auto param = GetParam();
+                    const auto result = cdl::loot::parse::LootLineParser{}.parse(param.line);
+                    ASSERT_EQ(result.monster, param.expectedMonsterName);
+                }
+
+                INSTANTIATE_TEST_CASE_P(LootLineParser, MonsterNameTest,
+                                        testing::Values(
+                                        MonsterNameTestStruct{
+                                            std::string{ "Loot of a monster: " },
+                                            std::string{ "monster" }
+                                        },
+                                        MonsterNameTestStruct{
+                                            std::string{ "Loot of an Long Monster Name: " },
+                                            std::string{ "Long Monster Name" }
+                                        },
+                                        MonsterNameTestStruct{
+                                            std::string{ "Loot of an Monster'something: " },
+                                            std::string{ "Monster'something" }
+                                        }));
+            }
         }
     }
-
 }
