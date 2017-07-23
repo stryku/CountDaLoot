5399d55108cea7b8fb93fdf212d4c2510fe34d99
stryku <stryku2393@gmail.com>
2017-07-23 21:45:00 +0200

    improved cloc

--- cloc.py
+++ cloc.py
@@ -1,2 +1,15 @@
-from subprocess import call
-call(["cloc.exe", ".", "--exclude-dir=tmp,resources,releases,cmake,build,bin,3rdparty"])
+from subprocess import Popen, PIPE
+
+def get_total_loc(output_lines):
+    for line in output_lines:
+        if line.startswith('SUM'):
+            clear_str = " ".join(line.split())
+            lines_counts = [int(x) for (x) in clear_str.split(' ')[-3:]]
+            return sum(lines_counts)
+            
+    return 0
+
+
+proc = Popen(["cloc", ".", "--exclude-dir=tmp,resources,releases,cmake,build,bin,3rdparty,data"], stdout=PIPE)
+loc = get_total_loc(proc.stdout.read().splitlines())
+print(loc)
